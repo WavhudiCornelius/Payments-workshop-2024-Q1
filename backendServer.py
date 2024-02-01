cc5a17af0fbe605f08c98220a97d2914a1a51371
Michelle Huggins <mich.huggins1@gmail.com>
2024-02-01 15:43:16 +0200

    add validate API

--- backendServer.py
+++ backendServer.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from payhealth_api_validate import payhealth_api_validate
 
 app = Flask(__name__)
+
+# Register the Blueprint
+app.register_blueprint(payhealth_api_validate)
 
 @app.route('/')
 def hello():
