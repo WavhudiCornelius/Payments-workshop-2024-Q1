939139b94529f00621d5853b583a499b19ccea34
Liam Stander <liam.stander@impact.com>
2024-02-01 15:13:04 +0200

    BE005: Endpoint

--- backendServer.py
+++ backendServer.py
@@ -6,5 +6,29 @@ app = Flask(__name__)
 def hello():
     return 'Hello, World!'
 
+@app.route('/api/payhealth/reporting', methods=['GET'])
+def reporting_endpoint():
+
+    field = request.args.get('field')
+    error = request.args.get('error')
+
+
+    cursor.execute(f'''
+        SELECT *
+        FROM error_info
+        WHERE error_type = {error} AND field_type = {field} 
+    ''')
+    
+    rows = cursor.fetchall()
+
+
+    filtered_data = [
+        {'id': row[0], 'date': row[1], 'field': row[2], 'error': error}
+        for row in rows
+    ]
+
+    return jsonify(filtered_data)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
